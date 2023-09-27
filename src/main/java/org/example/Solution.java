638eab1651d93ddc5cdfbc4a95963a53a333f666
Natalia <nataly.konovalenko@gmail.com>
2023-09-27 17:47:50 +0300

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,8 +1,22 @@
 package org.example;
 
-public class Solution {
+class Solution {
     public String longestCommonPrefix(String[] strs) {
-        //write your code here
-        return "";
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String commonPrefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(commonPrefix) != 0) {
+                commonPrefix = commonPrefix.substring(0, commonPrefix.length() - 1);
+                if (commonPrefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return commonPrefix;
     }
 }
+
